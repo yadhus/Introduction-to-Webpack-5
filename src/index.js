cbe6d97b6b2a517c2730a9dbe7837676791e1827
Yadhunandan S <yadhus@users.noreply.github.com>
2021-01-09 18:15:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./main.css";
+
+console.log("Introduction to Webpack 5");
+
+let a = 10;
+let b = 30;
+
+let c = a + b;
+
+console.log("Adding 10 + 30 = ", c);
+
+setTimeout(function alerting() {
+  alert("Hello World");
+}, 500);
